f7b5625dda81d93d4004deb7e005448e49fdb6ae
ivanmunoz99 <ivandariomunos99@gmail.com>
2022-11-15 18:19:36 -0500

    draw canva

---
+++ tetris.js
@@ -0,0 +1,11 @@
+//we connect with the html id
+const canvas = document.getElementById("tetris");
+const fig = canvas.getContext("2d");
+const score = document.getElementById("score");
+
+// color of an empty square and the size of the canvas
+const fila = 20;
+const columna = 10;
+const cuadrado = 20;
+const vacant = "WHITE";
+

41c1362569136dafad21e9dd0e4cd986d554dc07
ivanmunoz99 <ivandariomunos99@gmail.com>
2022-11-16 13:19:36 -0500

    draw board

--- tetris.js
+++ tetris.js
@@ -5,7 +5,38 @@ const score = document.getElementById("score");
 
 // color of an empty square and the size of the canvas
 const fila = 20;
-const columna = 10;
-const cuadrado = 20;
-const vacant = "WHITE";
+const columna = column = 10;
+const cuadrado = square = 20;
+const vacio = "white";
 
+// each square
+function drawCuadrado (x,y,color){
+    fig.fillStyle = color;
+    fig.fillRect(x*cuadrado,y*cuadrado,cuadrado,cuadrado);
+    
+    fig.strokeStyle = "black";
+    fig.strokeRect(x*cuadrado,y*cuadrado,cuadrado,cuadrado);
+}
+
+// board array
+let board = [];
+
+// create the rows
+for (f = 0; f <fila; f++){
+    board[f] = [];
+    //create the columns
+    for (c = 0; c <columna; c++){
+        // each square is empty
+        board[f][c] = vacio;
+    }
+}
+
+// draw board
+function drawTablero(){
+    for (f = 0; f <fila; f++){
+        for (c = 0; c <columna; c++){
+            drawCuadrado(c,f,board[f][c]);
+        }
+    }
+}
+drawTablero();
